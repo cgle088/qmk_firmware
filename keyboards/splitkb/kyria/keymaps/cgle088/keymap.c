70f3e47c4511a3df442328cd48928f797e674a28
Clint Glenn <clintyfresh163@yahoo.com>
2022-01-01 14:07:32 -0600

    oled stuff

--- keyboards/splitkb/kyria/keymaps/cgle088/keymap.c
+++ keyboards/splitkb/kyria/keymaps/cgle088/keymap.c
@@ -278,6 +278,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // clang-format on
         oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
     }*/
+
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
     // Host Keyboard Layer Status
@@ -287,12 +288,12 @@ bool oled_task_user(void) {
         case _DVORAK:
             oled_write_P(PSTR("Default\n"), false);
             break;
-        // case _FN:
-        //     oled_write_P(PSTR("FN\n"), false);
-        //     break;
-        // case _ADJ:
-        //     oled_write_P(PSTR("ADJ\n"), false);
-        //     break;
+        case _SYM:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case _NAV:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
             oled_write_ln_P(PSTR("Undefined"), false);
@@ -306,6 +307,35 @@ bool oled_task_user(void) {
 
     return false;
 }
+#endif
+// #ifdef OLED_ENABLE
+// bool oled_task_user(void) {
+//     // Host Keyboard Layer Status
+//     oled_write_P(PSTR("Layer: "), false);
+
+//     switch (get_highest_layer(layer_state)) {
+//         case _DVORAK:
+//             oled_write_P(PSTR("Default\n"), false);
+//             break;
+//         // case _FN:
+//         //     oled_write_P(PSTR("FN\n"), false);
+//         //     break;
+//         // case _ADJ:
+//         //     oled_write_P(PSTR("ADJ\n"), false);
+//         //     break;
+//         default:
+//             // Or use the write_ln shortcut over adding '\n' to the end of your string
+//             oled_write_ln_P(PSTR("Undefined"), false);
+//     }
+
+//     // Host Keyboard LED Status
+//     led_t led_state = host_keyboard_led_state();
+//     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+//     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+//     oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+//     return false;
+// }
 // oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 //   if(!is_keyboard_master()){
 //      return OLED_ROTATION_180;
@@ -332,7 +362,7 @@ bool oled_task_user(void) {
     //     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
     //     oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
 // }
-#endif
+// #endif
 
 /*layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
@@ -501,7 +531,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
     case KC_BSPC:{
         //https://beta.docs.qmk.fm/using-qmk/simple-keycodes/feature_advanced_keycode
-        bool delkey_registered;
+        bool delkey_registered = false;
         if(record->event.pressed){
           if(mod_state & MOD_MASK_SHIFT){
             del_mods(MOD_MASK_SHIFT);
